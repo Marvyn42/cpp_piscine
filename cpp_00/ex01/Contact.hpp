67a0f85efe3ba70f61d23704775f98148cf24baf
mamaquig <marvyndany@gmail.com>
2022-01-15 02:27:32 +0100

    files cpp00 ex02 addded

--- cpp_00/ex01/Contact.hpp
+++ cpp_00/ex01/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: mamaquig <mamaquig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 14:13:45 by mamaquig          #+#    #+#             */
-/*   Updated: 2022/01/14 15:52:54 by mamaquig         ###   ########.fr       */
+/*   Updated: 2022/01/14 22:06:09 by mamaquig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ class Contact
 		
 		void		print_contacts(unsigned int index, Contact contact[8]);
 		bool		print_infos(unsigned int index, Contact contact[8]) const ;
+		
 		std::string	get_first_name(Contact contact) const;
 		std::string	get_last_name(Contact contact) const;
 		std::string	get_nickname(Contact contact) const;
--- cpp_00/ex02/Account.cpp
+++ cpp_00/ex02/Account.cpp
@@ -6,8 +6,53 @@
 /*   By: mamaquig <mamaquig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 03:24:29 by mamaquig          #+#    #+#             */
-/*   Updated: 2022/01/14 03:24:37 by mamaquig         ###   ########.fr       */
+/*   Updated: 2022/01/14 23:33:53 by mamaquig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+
+Account::Account( void ) {
+	//Initialiser l'affichage
+	//std::cout << [horodatage] << " index:" << 
+}
+
+Account::Account( int initial_deposit ) {
+}
+
+Account::~Account( void ) {
+	//afficher:
+	//[horodatage] index;amount;closed
+}
+
+static int	getNbAccounts( void ) {
+}
+
+static int	getTotalAmount( void ) {
+}
+
+static int	getNbDeposits( void ) {
+}
+
+static int	getNbWithdrawals( void ) {
+}
+
+// displayAccountsInfos affichera:
+// [horodatage] _nbAccounts;_totalAmount;_totalNbDeposits;_totalNbWithdrawals
+static void	displayAccountsInfos( void ) {
+}
+
+static void	_displayTimestamp( void ) {
+}
+
+void	makeDeposit( int deposit ) {
+}
+
+bool	makeWithdrawal( int withdrawal ) {
+}
+
+int		checkAmount( void ) const {
+}
+
+void	displayStatus( void ) const {
+}
