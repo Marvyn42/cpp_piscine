d6517b83bde2a2c373f69cf2496fba158cff20d5
mamaquig <marvyndany@gmail.com>
2022-02-22 04:00:30 +0100

    ex02 cpp07 added.

--- cpp_07/ex02/Array.hpp
+++ cpp_07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: mamaquig <mamaquig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 02:31:06 by mamaquig          #+#    #+#             */
-/*   Updated: 2022/02/22 02:33:02 by mamaquig         ###   ########.fr       */
+/*   Updated: 2022/02/22 03:59:52 by mamaquig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,35 @@
 
 # include <iostream>
 # include <stdlib.h>
+# include <stdint.h>
+# include <stdexcept>
 
-template< typename T >
+template< typename T = int >
 class Array {
 	public:
-		Array();
-		~Array();
+		Array<T>() : _elem(0), _size(0) {}
+		Array<T>(unsigned int n) : _elem(new T[n]), _size(n) {}
+		Array<T>(Array<T> const & cpy) {*this = cpy;}
+
+		virtual ~Array<T>() {delete[] _elem;}
+
+		Array<T> & operator=(Array<T> const & ref) {
+			_size = ref.size();
+			_elem = new T[_size];
+			for (size_t i = 0; i < _size; i++)
+				_elem[i] = ref._elem[i];
+			return (*this);}
+
+		T & operator[](size_t idx) {
+			if (idx >= _size)
+				throw std::out_of_range("Index insn't valid, it's out of range.");
+			return (this->_elem[idx]);}
+
+		size_t	size() const {return (this->_size);}
+
 	private:
+		T		*_elem;
+		size_t	_size;
 };
 
 #endif
--- cpp_07/ex02/main.cpp
+++ cpp_07/ex02/main.cpp
@@ -6,12 +6,50 @@
 /*   By: mamaquig <mamaquig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 02:31:10 by mamaquig          #+#    #+#             */
-/*   Updated: 2022/02/22 02:32:05 by mamaquig         ###   ########.fr       */
+/*   Updated: 2022/02/22 03:59:58 by mamaquig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 
-int	main(void) {
+int main(void) {
+	Array<int> empty;
+	std::cout << "Empty Tab as a size of " << empty.size() << std::endl;
+
+	std::cout << std::endl;
+
+	Array<int> tab(5);
+	tab[0] = 0;
+	tab[1] = 1;
+	tab[2] = 2;
+	tab[3] = 3;
+	tab[4] = 4;
+
+	std::cout << "Tab as a size of " << tab.size() << std::endl;
+
+	for (uint8_t i = 0; i < tab.size(); i++)
+		std::cout << tab[i] << std::endl;
+	std::cout << std::endl;
+
+	// Testing assignation
+	Array<int> tab_assign = tab;
+	std::cout << "Tab Assign as a size of " << tab_assign.size()
+			  << std::endl;
+	for (uint8_t i = 0; i < tab_assign.size(); i++)
+		std::cout << tab_assign[i] << std::endl;
+	std::cout << std::endl;
+
+	// Testing copy
+	Array<int> tab_cpy(tab);
+	std::cout << "Tab Copy as a size of " << tab_cpy.size() << std::endl;
+	for (uint8_t i = 0; i < tab_cpy.size(); i++)
+		std::cout << tab_cpy[i] << std::endl;
+	std::cout << std::endl;
+
+	// Testing out of range
+	try {
+		std::cout << "3rd element of tab is : " << tab[2] << std::endl;
+		std::cout << "10th element of tab is : " << tab[9] << std::endl;
+	} catch (std::exception const& e) { std::cerr << e.what() << std::endl; }
 	return (EXIT_SUCCESS);
 }
